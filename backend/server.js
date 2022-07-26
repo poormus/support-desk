f8f03e3e938672ee4ab09827465e01158a8b5c06
poormus <poormus@hotmail.com>
2022-07-26 14:25:41 +0200

    fixes

--- backend/server.js
+++ backend/server.js
@@ -27,9 +27,9 @@ if (process.env.NODE_ENV === 'development') {
     app.use(express.static(path.join(__dirname, '../frontend/build')))
   
     // FIX: below code fixes app crashing on refresh in deployment
-    // app.get('*', (_, res) => {
-    //   res.sendFile(path.join(__dirname, '../frontend/build/index.html'))
-    // })
+    app.get('*', (_, res) => {
+      res.sendFile(path.join(__dirname, '../frontend/build/index.html'))
+    })
   } else {
     app.get('/', (req, res) => {
       res.status(200).json({ message: 'Welcome to the Support Desk API' })
