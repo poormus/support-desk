008de7411352891037fc1f373a17303639aea223
poormus <poormus@hotmail.com>
2022-07-26 14:14:00 +0200

    change to development

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ app.use('/api/users',require('./routes/UserRoutes'))
 app.use('/api/tickets',require('./routes/TicketRoutes'))
 
 // Serve Frontend
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === 'development') {
     // Set build folder as static
     app.use(express.static(path.join(__dirname, '../frontend/build')))
   
